d6dcc1618bbba06941b3d9206f02c1468e5966fe
Gus Brodman <gbrodman@google.com>
2019-05-21 14:12:47 -0400

    Refactor to be more in line with a standard Gradle project structure

--- release/build_nomulus_for_env.sh
+++ release/build_nomulus_for_env.sh
@@ -31,28 +31,24 @@ if [ "${environment}" == tool ]
 then
   mkdir -p "${dest}"
 
-  cd gradle
   ./gradlew clean :core:nomulus \
     -PmavenUrl=https://"${gcs_prefix}"/maven \
     -PpluginsUrl=https://"${gcs_prefix}"/plugins
-  cd -
 
   mv gradle/core/build/libs/nomulus.jar .
 else
   mkdir -p "${dest}"
 
-  cd gradle
   ./gradlew clean stage -Penvironment="${environment}" \
     -PmavenUrl=https://"${gcs_prefix}"/maven \
     -PpluginsUrl=https://"${gcs_prefix}"/plugins
-  cd -
 
   for service in default pubapi backend tools
   do
-    mv gradle/services/"${service}"/build/staged-app "${dest}/${service}"
+    mv services/"${service}"/build/staged-app "${dest}/${service}"
   done
 
-  mv gradle/core/build/resources/main/google/registry/env/common/META-INF \
+  mv core/build/resources/main/google/registry/env/common/META-INF \
     "${dest}/META-INF"
 
   cd "${dest}"
